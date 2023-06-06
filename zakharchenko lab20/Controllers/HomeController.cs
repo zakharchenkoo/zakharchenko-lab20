39a3a405664c116c13d952e013f7cb6600e1b6bb
zakharchenko <a.zakharchenko2004@gmail.com>
2023-06-06 22:53:37 +0200

    first commit

---
+++ zakharchenko lab20/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using zakharchenko_lab20.Models;
+
+namespace zakharchenko_lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CountLines(string inputText)
+        {
+            int lineCount = CountLinesInText(inputText);
+            ViewBag.Result = "Кількість рядків: " + lineCount.ToString();
+            return View("Index");
+        }
+
+        private int CountLinesInText(string inputText)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                count = inputText.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Length;
+            }
+            return count;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ zakharchenko lab20/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace zakharchenko_lab20.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
